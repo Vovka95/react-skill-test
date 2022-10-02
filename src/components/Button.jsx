50a3352817c891f389c97fa720a2d5796f92a07e
vprotskyi <vprotskyi@tsukat.com>
2022-10-02 11:47:25 +0300

    turn the button into a reusable component

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ className, title }) => {
+    return <button className={className}>{title}</button>;
+};
+
+export default Button;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { timeActions } from "../store/time";
+
 import TimeSpend from "./TimeSpend";
+import Button from "./Button";
 
 const HomePage = () => {
     const dispatch = useDispatch();
@@ -17,10 +19,10 @@ const HomePage = () => {
         <div className="w-full h-screen flex flex-col justify-center items-center">
             <h1>Welcome to Ucan</h1>
             <TimeSpend className="my-4" />
-
-            <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ripple-surface-light">
-                Toggle
-            </button>
+            <Button
+                title="toggle"
+                className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ripple-surface-light"
+            />
         </div>
     );
 };
