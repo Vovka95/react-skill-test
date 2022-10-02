9bc81fcec1b6b28e91d59719055c4695d36261d4
vprotskyi <vprotskyi@tsukat.com>
2022-10-02 11:42:36 +0300

    fix timer increase

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,21 +4,25 @@ import { timeActions } from "../store/time";
 import TimeSpend from "./TimeSpend";
 
 const HomePage = () => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			dispatch(timeActions.updateTime(1));
-		}, 1000);
-	}, []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            dispatch(timeActions.updateTime(1));
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+            <h1>Welcome to Ucan</h1>
+            <TimeSpend className="my-4" />
 
-	return <div className="w-full h-screen flex flex-col justify-center items-center">
-		<h1>Welcome to Ucan</h1>
-		<TimeSpend className='my-4' />
-
-		<button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ripple-surface-light">Toggle</button>
-	</div>
-}
+            <button className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out ripple-surface-light">
+                Toggle
+            </button>
+        </div>
+    );
+};
 
 export default HomePage;
